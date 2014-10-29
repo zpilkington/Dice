8d1bf4874f79b821a44a6805830acffcfe20cadd
zach <zach@thundercock>
2014-10-28 19:47:45 -0500

    first commit

---
+++ src/dice/Dice.java
@@ -0,0 +1,20 @@
+/*
+ * This thingy was made by Zach Pilkington
+ */
+
+package dice;
+
+/**
+ *
+ * @author zach
+ */
+public class Dice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
